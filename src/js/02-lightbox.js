037d36dfdfd2889168af169979542e76308d4913
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-10-22 18:30:52 +0300

    task-2 correction

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,15 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
 
-const imageArray = galleryItems.map(({ original, preview, description }) => {
-  return `<li class="gallery__item">
+const imageArray = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<li class="gallery__item">
       <a class="gallery__link" href="${original}">
          <img class="gallery__image" src="${preview}"
-            "alt="${description}"
+            alt="${description}"
           />
       </a>
     </li>`;
-});
-galleryEl.insertAdjacentHTML('afterbegin', imageArray.join(''));
+  })
+  .join('');
 
-new SimpleLightbox('.gallery a', {});
+galleryEl.insertAdjacentHTML('afterbegin', imageArray);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
